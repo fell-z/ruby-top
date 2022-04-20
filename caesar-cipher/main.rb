d859b49b84ff8efc147d32a374aaf633f754b52e
fell-z <matheusfelipeagapito@hotmail.com>
2022-04-19 21:07:32 -0300

    Refactor caesar-cipher code to be more clean

--- caesar-cipher/main.rb
+++ caesar-cipher/main.rb
@@ -4,24 +4,19 @@ def caesar_cipher(text, shift_factor)
   uppercase_alphabet = ("A".."Z").to_a
 
   ciphered_text = text.split("").map do |char|
-    if uppercase_alphabet.include? char
-      ciphered_letter_index = uppercase_alphabet.index(char) + shift_factor
-      # wrapping 'Z' to 'A'
-      ciphered_letter_index -= numbers_of_letters if ciphered_letter_index >= numbers_of_letters
-      # wrapping 'A' to 'Z' (backwards 0 -> 26)
-      ciphered_letter_index = numbers_of_letters + ciphered_letter_index if ciphered_letter_index.negative?
+    next char unless lowercase_alphabet.include?(char) || uppercase_alphabet.include?(char)
 
-      uppercase_alphabet[ciphered_letter_index]
-    elsif lowercase_alphabet.include? char
-      ciphered_letter_index = lowercase_alphabet.index(char) + shift_factor
-      # wrapping 'z' to 'a'
-      ciphered_letter_index -= numbers_of_letters if ciphered_letter_index >= numbers_of_letters
-      # wrapping 'A' to 'Z' (backwards 0 -> 26)
-      ciphered_letter_index = numbers_of_letters + ciphered_letter_index if ciphered_letter_index.negative?
+    ciphered_letter_index = (uppercase_alphabet.index(char) || lowercase_alphabet.index(char)) + shift_factor
+    # wrapping 'Z' to 'A'
+    ciphered_letter_index -= numbers_of_letters if ciphered_letter_index >= numbers_of_letters
+    # wrapping 'A' to 'Z' (backwards 0 -> 26)
+    ciphered_letter_index = numbers_of_letters + ciphered_letter_index if ciphered_letter_index.negative?
 
+    case char
+    when char.downcase
       lowercase_alphabet[ciphered_letter_index]
-    else
-      char
+    when char.upcase
+      uppercase_alphabet[ciphered_letter_index]
     end
   end
   ciphered_text.join
@@ -35,9 +30,7 @@ print "Now, the shift factor, negative numbers for left shift, otherwise positiv
 input_shift_factor = gets.chomp.to_i
 puts "\n"
 
-if input_text.empty? || !input_shift_factor.is_a?(Integer)
-  raise TypeError, "No text or invalid shift factor (Must be a integer!)"
-end
+raise TypeError, "No text or invalid shift factor!" if input_text.empty? || input_shift_factor.eql?(0)
 
 result = caesar_cipher(input_text, input_shift_factor)
 puts "Result: #{result}"
