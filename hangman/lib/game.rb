3a11166b8699b390a9b1b92adc13be10730e05cf
fell-z <matheusfelipeagapito@hotmail.com>
2022-07-24 19:15:07 -0300

    Update hangman game
    Add base methods to the game class.
    Make the game init method load and select a word from the dictionary.
    Make the game display a welcome message

--- hangman/lib/game.rb
+++ hangman/lib/game.rb
@@ -1,4 +1,33 @@
 # Hangman game
 class Hangman
-  def initialize() end
+  def initialize
+    dictionary = File.readlines("lib/dictionary.txt").map(&:chomp).select { |word| word.length.between?(5, 12) }
+    @word = dictionary.sample
+
+    welcome_message
+  end
+
+  private
+
+  def game() end
+
+  def ask() end
+
+  def render() end
+
+  def find_saves() end
+
+  def load_save?() end
+
+  def welcome_message
+    puts <<~WELCOME
+      Hello! This is a game of hangman written in the Ruby lang.
+      The game is basically the original game, with no funny modifications made.
+
+      A word will be selected randomly and you will have 10 attempts to guess the word,
+      but you can also try to guess the word directly.
+
+      Obs: You can save the game to later load and resume your session.
+    WELCOME
+  end
 end
