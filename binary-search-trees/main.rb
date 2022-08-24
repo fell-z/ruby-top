d2dc22f25fde6b6a29cfea506bae4285655855d4
fell-z <matheusfelipeagapito@hotmail.com>
2022-08-23 22:07:38 -0300

    Update BST project
    Made the Tree object build itself on initialization.

--- binary-search-trees/main.rb
+++ binary-search-trees/main.rb
@@ -1,4 +1,7 @@
+# Binary Search Tree implementation
 class Tree
+  attr_reader :root
+
   # Tree node who stores data as well the left and right children nodes.
   class Node
     attr_accessor :data, :left, :right
@@ -16,5 +19,23 @@ class Tree
     rescue StandardError
       nil
     end
+  end
+
+  def initialize(arr)
+    @root = build_tree(arr)
+  end
+
+  private
+
+  def build_tree(arr)
+    return if arr.empty?
+
+    arr = arr.sort
+    mid_point = arr.length / 2
+    Node.new(
+      arr[mid_point],
+      build_tree(arr[0, mid_point]),
+      build_tree(arr[mid_point + 1, arr.length])
+    )
   end
 end
