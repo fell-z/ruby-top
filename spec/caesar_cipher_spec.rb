bfa3c988fd30809383585fb61c0e12933c1ef148
fell-z <matheusfelipeagapito@hotmail.com>
2022-09-30 19:47:33 -0300

    Add a test for the caesar cipher project
    Added a test to check if the caesar_cipher method can cipher using a
    left shift factor.
    Refactored some of the other tests to be a little more concise.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,19 +1,29 @@
 require_relative "../caesar-cipher/main"
 
 describe "caesar_cipher method" do
-  context "can cipher words with a right shift factor of 4" do
+  context "when is given a right shift factor of 4 (positive value)" do
     let(:shift_factor) { 4 }
 
-    it 'returns "Hello" ciphered' do
-      ciphered_text = caesar_cipher("Hello", shift_factor)
+    it 'returns "Hi mom" ciphered' do
+      ciphered_text = caesar_cipher("Hi mom", shift_factor)
 
-      expect(ciphered_text).to eq("Lipps")
+      expect(ciphered_text).to eq("Lm qsq")
     end
 
-    it 'returns "This is a secret message." ciphered without changing the spaces or punctuation.' do
-      ciphered_text = caesar_cipher("This is a secret message.", shift_factor)
+    it 'returns "Hello, world!" ciphered only changing the letters' do
+      ciphered_text = caesar_cipher("Hello, world!", shift_factor)
 
-      expect(ciphered_text).to eq("Xlmw mw e wigvix qiwweki.")
+      expect(ciphered_text).to eq("Lipps, asvph!")
+    end
+  end
+
+  context "when is given a left shift factor of 6 (negative value)" do
+    let(:shift_factor) { -6 }
+
+    it 'returns "Hi mom" ciphered' do
+      ciphered_text = caesar_cipher("Hi mom", shift_factor)
+
+      expect(ciphered_text).to eq("Bc gig")
     end
   end
 end
