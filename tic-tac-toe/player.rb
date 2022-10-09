74f24616641f0f3b0f569e99ba7a9ea519f8e70f
fell-z <matheusfelipeagapito@hotmail.com>
2022-10-09 12:48:17 -0300

    Update TicTacToe project
    - Added method that asks the player to choose a square and returns
    that square if it's valid
    - Changed some output text(grammar, etc)
    - Removed redundant array conversion method

--- tic-tac-toe/player.rb
+++ tic-tac-toe/player.rb
@@ -8,15 +8,25 @@ class Player
     @plays = []
   end
 
+  def make_a_play(possible_squares)
+    loop do
+      print "Your turn, #{@name}. Select a empty square.\n>> "
+      play = gets.chomp.to_i
+      break play if (1..9).include?(play) && possible_squares.include?(play)
+
+      puts "\nOops! Invalid input or square already taken."
+    end
+  end
+
   private
 
   def set_name
     loop do
-      print "Please, insert your name starting with a letter.\n>> "
+      print "Insert your name starting with a letter.\n>> "
       name = gets.chomp
-      break name if ("a".."z").to_a.include?(name[0].downcase)
+      break name if ("a".."z").include?(name[0].downcase)
 
-      puts "\nInput error: name not starting with letter."
+      puts "\nOops! Name not starting with a letter."
     end
   end
 end
