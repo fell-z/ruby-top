53e51202c6bcd01dc30d8a90fd644a13440b0ab0
fell-z <matheusfelipeagapito@hotmail.com>
2022-10-09 11:39:54 -0300

    Update TicTacToe project
    - Added constant with the valid combinations to win a game

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -1,5 +1,11 @@
 # TicTacToe game
 class TicTacToe
+  VALID_COMBINATIONS = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], # horizonal
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], # vertical
+    [1, 5, 9], [3, 5, 7] # diagonal
+  ].freeze
+
   def initialize(player_one, player_two)
     @board = [
       1, 2, 3,
