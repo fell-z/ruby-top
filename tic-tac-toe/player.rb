1a789554798ca403ff34b15d1d9e144b68c8a899
fell-z <matheusfelipeagapito@hotmail.com>
2022-10-09 12:11:23 -0300

    Update TicTacToe project
    - Added constructor method
    - Added method to set the player's name

--- tic-tac-toe/player.rb
+++ tic-tac-toe/player.rb
@@ -1,3 +1,22 @@
 # TicTacToe player
 class Player
+  attr_reader :name, :points, :plays
+
+  def initialize
+    @name = set_name
+    @points = 0
+    @plays = []
+  end
+
+  private
+
+  def set_name
+    loop do
+      print "Please, insert your name starting with a letter.\n>> "
+      name = gets.chomp
+      break name if ("a".."z").to_a.include?(name[0].downcase)
+
+      puts "\nInput error: name not starting with letter."
+    end
+  end
 end
